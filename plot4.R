d03b1c395d850e6cee0ce883414f2867b6f6a3d6
Siu Ching Tan <66198513+SCTAN-Msia@users.noreply.github.com>
2021-11-15 23:00:57 +0800

    Update plot4.R
    Missing Content in my code

--- plot4.R
+++ plot4.R
@@ -1,3 +1,17 @@
+# Project Name: Conduct a preliminary graphic analysis on individual household electric power consumption.
+# Data: Data set from the UC Irvine Machine Learning Repository. 
+# The data measurements of electric power consumption in one household with a one-minute sampling 
+# rate over a period of almost 4 years. Different electrical quantities and some sub-metering values are available.
+
+# Construct Plot 4
+
+#setwd working directory
+setwd("C:/Users/small/Documents/R_GitHub/EDA/EDA Project 1")
+
+#Reads in data from the working directory
+EPowerDat <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+
+# convert Date variable to Date class
 EPowerDat$Date <- as.Date(EPowerDat$Date, "%d/%m/%Y")
 
 # Filter Subset data from 2007-02-01 and 2007-02-02
